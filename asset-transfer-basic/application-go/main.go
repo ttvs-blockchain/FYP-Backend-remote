a1ad31c3d1374ea1a721714b5ec68af5fb28f51d
lwuar <lwuar@connect.ust.hk>
2022-02-22 20:09:01 +0800

    debug

--- asset-transfer-basic/application-go/main.go
+++ asset-transfer-basic/application-go/main.go
@@ -76,53 +76,6 @@ func main() {
 
 	contract := network.GetContract("basic")
 
-	// log.Println("--> Submit Transaction: InitLedger, function creates the initial set of assets on the ledger")
-	// result, err := contract.SubmitTransaction("InitLedger")
-	// if err != nil {
-	// 	log.Fatalf("Failed to Submit transaction: %v", err)
-	// }
-	// log.Println(string(result))
-
-	// log.Println("--> Evaluate Transaction: GetAllAssets, function returns all the current assets on the ledger")
-	// result, err = contract.EvaluateTransaction("GetAllAssets")
-	// if err != nil {
-	// 	log.Fatalf("Failed to evaluate transaction: %v", err)
-	// }
-	// log.Println(string(result))
-
-	// log.Println("--> Submit Transaction: CreateAsset, creates new asset with ID, color, owner, size, and appraisedValue arguments")
-	// result, err = contract.SubmitTransaction("CreateAsset", "asset13", "yellow", "5", "Tom", "1300")
-	// if err != nil {
-	// 	log.Fatalf("Failed to Submit transaction: %v", err)
-	// }
-	// log.Println(string(result))
-
-	// log.Println("--> Evaluate Transaction: ReadAsset, function returns an asset with a given assetID")
-	// result, err = contract.EvaluateTransaction("ReadAsset", "asset13")
-	// if err != nil {
-	// 	log.Fatalf("Failed to evaluate transaction: %v\n", err)
-	// }
-	// log.Println(string(result))
-
-	// log.Println("--> Evaluate Transaction: AssetExists, function returns 'true' if an asset with given assetID exist")
-	// result, err = contract.EvaluateTransaction("AssetExists", "asset1")
-	// if err != nil {
-	// 	log.Fatalf("Failed to evaluate transaction: %v\n", err)
-	// }
-	// log.Println(string(result))
-
-	// log.Println("--> Submit Transaction: TransferAsset asset1, transfer to new owner of Tom")
-	// _, err = contract.SubmitTransaction("TransferAsset", "asset1", "Tom")
-	// if err != nil {
-	// 	log.Fatalf("Failed to Submit transaction: %v", err)
-	// }
-
-	// log.Println("--> Evaluate Transaction: ReadAsset, function returns 'asset1' attributes")
-	// result, err = contract.EvaluateTransaction("ReadAsset", "asset1")
-	// if err != nil {
-	// 	log.Fatalf("Failed to evaluate transaction: %v", err)
-	// }
-	// log.Println(string(result))
 	log.Println("============ application-golang ends ============")
 
 	r := gin.Default()
@@ -168,7 +121,8 @@ func main() {
 
 		log.Println("--> Submit Transaction: CreateAsset, creates new asset with", json)
 
-		result, err := contract.SubmitTransaction("CreateAsset", json.AssetID, json.Color, json.Size, json.Owner, json.AppraisedValue)
+		result, err := contract.SubmitTransaction("CreateAsset",
+			json.CertNo, json.ID, json.Name, json.Brand, json.NumOfDose, json.Time, json.Issuer, json.Remark)
 
 		if err != nil {
 			log.Println("Failed to Submit transaction: %v", err)
